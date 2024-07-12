62a4e2e9ba262f0010e33d32047042edfef77821
Birhan-Ayinalem <ayichewbirhanayinalem.com>
2024-07-12 19:07:57 +0300

    for deploy

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
+import Footer from '../../components/Footer/Footer'
+import RowList from '../../components/Row/RowList/RowList'
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
